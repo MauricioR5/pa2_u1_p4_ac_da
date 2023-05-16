5140f31feedf4c8c1832c993f974e961c31a7b77
Alexander Cacuango <amcacuangoa@uce.edu.ec>
2023-05-15 19:39:57 -0500

    Taller 4

--- src/main/java/com/example/demo/Pa2U1P4AcDaApplication.java
+++ src/main/java/com/example/demo/Pa2U1P4AcDaApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +19,20 @@ public class Pa2U1P4AcDaApplication implements CommandLineRunner {
 		System.out.println("Mi primer proyecto");
 		/*Spring framework: Framework que permite la construcción/desarrollo 
 		 * de aplicaciones empresariales basadas en Spring*/
-		}
+	Profesor profe = new Profesor();
+	profe.setNombre("Andrade");
+	profe.setApellido("Dennisse");
+	profe.setCedula("1751556653");
+	profe.setFechaNacimiento(LocalDateTime.now());
+	
+	Profesor profe2 = new Profesor();
+	profe2.setNombre("Mauricio");
+	profe2.setApellido("Cacuango");
+	profe2.setCedula("1723524664");
+	profe2.setFechaNacimiento(LocalDateTime.now());
+	
+	System.out.println(profe);
+	System.out.println(profe2);
+	}
 
 }
